394e0a7fa66c10840da4245de9b48bbbf01529be
EC2 Default User <ec2-user@ip-172-31-36-88.ap-northeast-1.compute.internal>
2023-11-24 19:34:46 +0000

    ~p26

---
+++ blank.rb
@@ -0,0 +1,10 @@
+puts (5+ 2   -  5)*   2
+
+def add(  a,      b)
+  a     +      b
+          end
+
+puts add(1      ,3)
+
+
+puts add(5,6)
---
+++ to_s.rb
@@ -0,0 +1,19 @@
+puts "1".to_s
+
+puts 1.to_s
+
+puts nil.to_s
+
+puts true.to_s
+
+puts false.to_s
+
+puts 11.to_s(16)
+
+puts 10.to_s 16
+
+puts 1.to_s; puts nil.to_s; puts 11.to_s(16)
+
+puts 10.to_s(
+  16
+  )
