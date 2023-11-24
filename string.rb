bad44f4cf8a8109bac0c5fb3e8de71241557717a
EC2 Default User <ec2-user@ip-172-31-36-88.ap-northeast-1.compute.internal>
2023-11-24 20:18:26 +0000

    ~p30

---
+++ string.rb
@@ -0,0 +1,28 @@
+puts 'これは文字列です'
+
+puts "これも文字列です"
+
+# 改行されない
+puts 'こんにちはさようなら'
+
+# ダブルクオート内の＼nは改行される
+puts "こんにちは\nさようなら"
+
+# シングルクオートのため改行されない
+puts 'こんにちは\nさようなら'
+
+
+# ダブルクオートの時のみ展開
+i = 10
+puts "#{i}は１６進数にすると#{i.to_s 16}です"
+
+name = 'Alice'
+puts "Hello, #{name}"
+
+# ＼の打ち消し
+puts "Hello, \#{name}"
+
+# エスケープ処理
+puts 'He said, "Don\'t speak."'
+
+puts "He said, \"Don't speak.\""
