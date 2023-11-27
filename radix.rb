2bd6e37f6f87efeab5fd36bc7696fa524086ded0
Shunmurama <mura.faily72@gmail.com>
2023-11-27 19:28:00 +0000

    ~p58

--- radix.rb
+++ radix.rb
@@ -1,1 +1,28 @@
+# ２進数
 puts 0b11111111
+
+# 8進数
+puts 0377
+
+# 16進数
+puts 0xff
+
+puts 2e-3
+
+puts 10.class
+
+puts 1.5.class
+
+puts "1".class
+
+r = 2/3r
+
+puts r
+
+puts r.class
+
+r = '2/3'.to_r
+
+puts r 
+
+puts r.class
